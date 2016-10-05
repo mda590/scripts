b6f791e76a4afb26788272f649828f1fd33e4529
Matt Adorjan <matt.adorjan@usfoods.com>
2016-10-05 09:03:12 -0500

    adding checkInt.ps1

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ This is a collection of different scripts I have created for different purposes,
 Most of them will be in the context of AWS.
 
 1) aws-ebs-deleteAvailableVolumes.py - This deletes EBS volumes which are in the 'available' state. It also checks against a single tag, and if a volume has that tag, the volume will not be deleted.
+
 2) checkInt.ps1 - This is a PowerShell script that polls the status of a NIC card on a Windows 2k12 server via UserData in CloudFormation. This can be used when a NIC card is not stabilizing fast enough and CloudFormation fails and rollsback due to the NIC card not initializing fast enough.
---
+++ checkInt.ps1
@@ -0,0 +1,23 @@
+﻿#
+#	This script handles an issue in AWS when trying to attach 2 NIC cards to a Windows 2012 R1 server.
+#	For whatever reason, this OS requires more time to recognize and install a second NIC card. This issue doesn't appear to occur in R2.
+#	When trying to provision a 2k12 R1 server via CloudFormation with 2 NICs, CloudFormation will time out and roll back the entire stack,
+#	stating that the NIC card could not be stabilized. This script when put into UserData will check the status of the 2nd NIC card for 
+#	1 extra minute, and allows enough time for the NIC card to stabilize, and allows the CloudFormation script to finish successfully.
+#
+#	September 12, 2016
+#
+
+$timeout = New-Timespan -Minutes 1          # Run the while loop for 1 minutes
+$stopwatch= [diagnostics.stopwatch]::StartNew()
+$IntStatus = (get-wmiobject win32_networkadapter -filter 'netconnectionid = "Ethernet 2"' | select netconnectionstatus) | Out-String
+$IntStatus
+while(($IntStatus -eq "")) {
+    $IntStatus = (get-wmiobject win32_networkadapter -filter 'netconnectionid = "Ethernet 2"' | select netconnectionstatus) | Out-String
+    if ($stopwatch.elapsed -gt $timeout) {
+        Write-Host "Breaking Now"
+        break
+    }
+    Start-Sleep -s 1         # Pause while loop for every 1 second
+}
+Write-Host "Timed out"
